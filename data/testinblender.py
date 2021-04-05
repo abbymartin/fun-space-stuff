691bc724fffe1234584ed08e8898926f3be38280
abbymartin <abbyvmartin51@gmail.com>
2021-04-05 19:01:17 -0400

    test dataset in blender

---
+++ data/testinblender.py
@@ -0,0 +1,16 @@
+import bpy 
+import csv
+
+# Settings 
+name = 'stars' 
+
+
+#get csv file
+with open("C:/Users/836844/Documents/starchart/visiblestars.csv", "r") as file:
+    reader = csv.reader(file)
+    next(reader)
+    #add spheres to screen
+    for row in reader:
+        print(row)
+        bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(float(row[17]), float(row[18]), float(row[19])))   
+
