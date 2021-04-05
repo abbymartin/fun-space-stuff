c7f18ab392aca8e7766e06d9c1ce7f9bae79bfac
abbymartin <abbyvmartin51@gmail.com>
2021-04-05 19:07:35 -0400

    add basic html and css

--- data/testinblender.py
+++ data/testinblender.py
@@ -1,4 +1,4 @@
-import bpy 
+#import bpy 
 import csv
 
 # Settings 
@@ -12,5 +12,5 @@ with open("C:/Users/836844/Documents/starchart/visiblestars.csv", "r") as file:
     #add spheres to screen
     for row in reader:
         print(row)
-        bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(float(row[17]), float(row[18]), float(row[19])))   
+        #bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(float(row[17]), float(row[18]), float(row[19])))   
 
