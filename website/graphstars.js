f37629fc492b409749d1b0b8cf873e52018c95c0
abbymartin <abbyvmartin51@gmail.com>
2021-04-11 13:42:02 -0400

    read json data in js

--- website/graphstars.js
+++ website/graphstars.js
@@ -1,8 +1,23 @@
 //open json file at ../data/visiblestarsjson.json to use in js
-console.log("test")
+console.log("test");
 
-//xml http reuqest?
-
+//fetch request
+function readFile(file) {
+    fetch(file)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error :( " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+    })
+    .catch(function () {
+        this.dataError = true;
+    })
+ }
 //print data 
+readFile('https://abbymartin.github.io/fun-space-stuff/data/visiblestarsjson.json');
 
 //add stuff to scene
