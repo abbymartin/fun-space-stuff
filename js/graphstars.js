4c2a852ec3e0dcb62a6549ac1896eafa3126c615
abbymartin <abbyvmartin51@gmail.com>
2021-04-13 09:23:05 -0400

    refactor code

--- js/graphstars.js
+++ js/graphstars.js
@@ -1,5 +1,55 @@
-//fetch request
+//get data from database and create map of stars
+
+/*TODO: 
+- moveable camera
+- effect when click on star
+- group constellations
+- search bar
+- add more stars (dynamic loading?)
+*/
+
 let url = 'https://abbymartin.github.io/fun-space-stuff/visiblestars.json'
+
+function addStars(data, scene) {
+  let stars = [];
+
+  const geometry = new THREE.SphereGeometry();
+  const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF})
+
+  //add spheres at star positions
+  for(let i = 0; i < data.length; i++) {
+    stars.push(new THREE.Mesh(geometry, material));
+
+    //set position and size
+    stars[i].position.x = data[i].x;
+    stars[i].position.y = data[i].y;
+    stars[i].position.z = data[i].z;
+    stars[i].scale.x = 0.1;
+    stars[i].scale.y = 0.1;
+    stars[i].scale.z = 0.1;
+
+    scene.add(stars[i]);
+  }
+}
+
+function drawScene(data) {
+  //three.js setup
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer();
+  
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  addStars(data, scene);
+
+  //set camera distance
+  camera.position.z = 100;
+
+  //render graphics (add animate here when needed)
+  renderer.render(scene, camera);
+}
 
 async function createMap() {
   //get json from url
@@ -11,40 +61,7 @@ async function createMap() {
   let data = await response.json(); 
   console.log(data);
 
-  //three.js setup
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  const renderer = new THREE.WebGLRenderer();
-  let stars = [];
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  const geometry = new THREE.SphereGeometry();
-  const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF})
-
-  //add spheres at star positions
-  for(let i = 0; i < data.length; i++) {
-    //add sphere to array
-    stars.push(new THREE.Mesh(geometry, material));
-
-    //set position and size
-    stars[i].position.x = data[i].x;
-    stars[i].position.y = data[i].y;
-    stars[i].position.z = data[i].z;
-    stars[i].scale.x = 0.1;
-    stars[i].scale.y = 0.1;
-    stars[i].scale.z = 0.1;
-
-    //add sphere to scene
-    scene.add(stars[i]);
-  }
-
-  //set camera distance
-  camera.position.z = 100;
-
-  //render graphics
-  renderer.render(scene, camera);
+  drawScene(data);
 }
 
 
